84422c1cbfd4bbb1873125afb570e5b72213018b
evanc1 <epciaga@gmail.com>
2019-10-14 11:40:00 -0500

    Update buildGrid.js

--- scripts/buildGrid.js
+++ scripts/buildGrid.js
@@ -1,5 +1,13 @@
 $.getJSON('/data/book.json', function(data) {         
-    alert(data);
+    var items = [];
+  $.each( data, function( key, val ) {
+    items.push( "<li id='" + key + "'>" + val + "</li>" );
+  });
+ 
+  $( "<ul/>", {
+    "class": "my-new-list",
+    html: items.join( "" )
+  }).appendTo( "body" );
 });
 
-console.log(tdata[1].title);
+
