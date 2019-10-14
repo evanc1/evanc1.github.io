694c8aa3e94691b4c46114ce06a9b1d5c9eded3e
evanc1 <epciaga@gmail.com>
2019-10-14 10:57:20 -0500

    Update buildGrid.js

--- scripts/buildGrid.js
+++ scripts/buildGrid.js
@@ -1,12 +1,3 @@
-var fs = require('fs'),
-    readline = require('readline');
-
-var rd = readline.createInterface({
-    input: fs.createReadStream('/path/to/file'),
-    output: process.stdout,
-    console: false
-});
-
-rd.on('line', function(line) {
-    console.log(line);
-})
+import * as data from 'data/book.json';
+const {name} = data;
+console.log(name); // output 'testing'
