06c19ce2c30220a4765f7a2797d9c569f99c80b2
evanc1 <epciaga@gmail.com>
2019-10-14 12:06:51 -0500

    Update buildGrid.js

--- scripts/buildGrid.js
+++ scripts/buildGrid.js
@@ -1,14 +1,20 @@
-$.getJSON('/data/book.json', function(data) {         
-    var items = [];
+var p = {
+    [
+{"title": "Tales from EarthSea",
+"author": "Ursula K. Le Guin",
+"price": "$5.99"}
+,{"title": "The Other Wind",
+"author": "Ursula K. Le Guin",
+"price": "$6.99"}
+,{"title": "The Left Hand of Darkness",
+"author":"Ursula K. Le Guin",
+"price":"$9.20"}
+    ]
+}
 
-  $.each( data, function( key, val ) {
-    items.push( "<li id='" + key + "'>" + val + "</li>" );
-  });
- 
-  $( "<ul/>", {
-    "class": "my-new-list",
-    html: items.join( "" )
-  }).appendTo( "body" );
-});
-
+for (var key in p) {
+    if (p.hasOwnProperty(key)) {
+        console.log(key + " -> " + p[key]);
+    }
+}
 
