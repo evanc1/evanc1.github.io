200d5ebc4a36938659b0f7a3c393ddc8302d94b1
evanc1 <epciaga@gmail.com>
2019-10-14 12:14:02 -0500

    Update buildGrid.js

--- scripts/buildGrid.js
+++ scripts/buildGrid.js
@@ -1,12 +1,12 @@
 var p = '{ "books" :
-    [ +
+    [' +
 '{"title": "Tales from EarthSea",
 "author": "Ursula K. Le Guin",
 "price": "$5.99"}
 ,' +{"title": "The Other Wind",
 "author": "Ursula K. Le Guin",
 "price": "$6.99"}
-,' +{"title": "The Left Hand of Darkness",
+,' + '{"title": "The Left Hand of Darkness",
 "author":"Ursula K. Le Guin",
 "price":"$9.20"}
     ]
