d135d1ec710b64bbdee9ce1d2c8ab442a444d6ed
evanc1 <epciaga@gmail.com>
2019-10-14 12:12:02 -0500

    Update buildGrid.js

--- scripts/buildGrid.js
+++ scripts/buildGrid.js
@@ -1,16 +1,16 @@
-var p = {
-    [
-{"title": "Tales from EarthSea",
+var p = '{ "books" :
+    [ +
+'{"title": "Tales from EarthSea",
 "author": "Ursula K. Le Guin",
 "price": "$5.99"}
-,{"title": "The Other Wind"
+,' +{"title": "The Other Wind",
 "author": "Ursula K. Le Guin",
 "price": "$6.99"}
-,{"title": "The Left Hand of Darkness",
+,' +{"title": "The Left Hand of Darkness",
 "author":"Ursula K. Le Guin",
 "price":"$9.20"}
     ]
-}
+}';
 
 for (var key in p) {
     if (p.hasOwnProperty(key)) {
